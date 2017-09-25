1ba4728c80a99a111173f57e016134f590cb836a
Renco Steenbergen <steenbergenrenco@gmail.com>
2017-09-25 23:23:11 +0200

    Change in path of application installation script

--- apm-vagrant/install-apm.sh
+++ apm-vagrant/install-apm.sh
@@ -4,7 +4,7 @@ echo '====================> APM application installation started <==============
 apt install maven -y
 cd /vagrant/apm-application
 mvn clean install
-cd apm-application/automated-personalized-mailer-web/target/
+cd automated-personalized-mailer-web/target/
 cp automated-personalized-mailer-web-1.0-SNAPSHOT.war /opt/wildfly/standalone/deployments/
 cd /vagrant/apm-application
 mvn clean
